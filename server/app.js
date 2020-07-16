6f8bca6b49cb025ec6d000a253a027a71bcc2f8e
Jorge <jorgeluisbizarroreyes@gmail.com>
2020-07-16 12:58:53 -0500

    improved server executing server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
-const { spawn } = require('child_process')
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const app = express()
+const port = 3000
 
 app.use(cors())
 app.use(express.json())
+
+const executeProgramPython = (name, params = []) => {
+    const { spawn } = require('child_process')
+    const program = path.join(__dirname, `script/${name}.py`)
+    return spawn('python3', [program, ...params])
+}
 
 app.post('/api/exec/register', (req, res) => {
     const {
@@ -12,10 +19,10 @@ app.post('/api/exec/register', (req, res) => {
         lastname
     } = req.body;
 
-    const programPython = spawn('py', ['src/register.py', name, lastname])
+    const programPython = executeProgramPython('register', [name, lastname])
 
     programPython.stdout.on('data', (data) => {
-        //res.write(data)
+        res.write(data)
         console.log(data.toString())
     })
 
@@ -31,10 +38,10 @@ app.post('/api/exec/recognize', (req, res) => {
         lastname
     } = req.body;
 
-    const programPython = spawn('py', ['src/recognize.py', name, lastname])
+    const programPython = executeProgramPython('recognize', [name, lastname])
 
     programPython.stdout.on('data', (data) => {
-        //res.write(data)
+        res.write(data)
         console.log(data.toString())
     })
 
@@ -44,6 +51,6 @@ app.post('/api/exec/recognize', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server run')
+app.listen(port, () => {
+    console.log(`server run on port ${port}`)
 })
